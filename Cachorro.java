f6246d2528ff9e0d7c5de850a28d806d1fb53106
AnaMilitz <63315782+AnaMilitz@users.noreply.github.com>
2022-11-14 23:54:39 -0300

    Update Cachorro.java

--- Cachorro.java
+++ Cachorro.java
@@ -2,4 +2,14 @@ public class Cachorro extends Animal {
   String pelagem;
   int porte;
   double peso;
+
+  public Cachorro(String nome, int idade, String cor, String pelagem, int porte, double peso, String raca) {
+    this.nome = nome;
+    this.idade = idade;
+    this.cor = cor;
+    this.pelagem = pelagem;
+    this.porte = porte;
+    this.peso = peso;
+    this.raca = raca;
+  } 
 }
