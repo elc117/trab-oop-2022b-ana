df34793857a636829f2f6d7eac5877267047c05e
AnaMilitz <63315782+AnaMilitz@users.noreply.github.com>
2022-11-16 00:23:01 -0300

    Update Gato.java

--- Gato.java
+++ Gato.java
@@ -1,5 +1,13 @@
 public class Gato extends Animal {
   String pelagem;
   int comprimento;
-  int espessura;
+
+   public Gato(String nome, int idade, String cor, String pelagem, int comprimento, String raca) {
+    this.nome = nome;
+    this.idade = idade;
+    this.cor = cor;
+    this.pelagem = pelagem;
+    this.comprimento = comprimento;
+    this.raca = raca;
+  } 
 }
