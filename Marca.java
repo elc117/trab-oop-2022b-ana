5787ccbdc67c332265c20c65414afcfbaf934d69
AnaMilitz <63315782+AnaMilitz@users.noreply.github.com>
2022-11-18 03:39:18 -0300

    Update Marca.java

--- Marca.java
+++ Marca.java
@@ -1,9 +1,29 @@
 public class Marca {
   String nome;
   String link;
+  boolean fazTestes;
 
-  public Marca(String nome, String link) {
+  public Marca(String nome, boolean fazTestes) {
     this.nome = nome;
+    this.link = "";
+    this.fazTestes = fazTestes;
+  }
+  
+  public Marca(String nome, boolean fazTestes, String link) {
+    this.nome = nome;
+    this.fazTestes = fazTestes;
     this.link = link;
+  }
+
+  public String getNome(){
+    return this.nome;
+  }
+
+    public String getLink(){
+    return this.link;
+  }
+  
+  public boolean fazTestesEmAnimais(){
+    return this.fazTestes;
   }
 }
